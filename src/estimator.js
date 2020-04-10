13057128b54de59e55c90c98acf1b8e3634327f6
Faith Idowu <faithid10@gmail.com>
2020-04-10 05:39:28 +0100

    Feature: Challenge 1, 2 completed?

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,9 @@
-const covid19ImpactEstimator = (data) => data;
+import { estimator } from './utils';
+
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: estimator({ ...data }, 10),
+  severeImpact: estimator({ ...data }, 50)
+});
 
 export default covid19ImpactEstimator;
---
+++ src/utils.js
@@ -0,0 +1,50 @@
+// eslint-disable-next-line import/prefer-default-export
+export const calculateLockDownDays = (periodType, timeToElapse) => {
+  switch (periodType) {
+    case 'months':
+      return timeToElapse * 30;
+    case 'weeks':
+      return timeToElapse * 7;
+    default:
+      return timeToElapse;
+  }
+};
+
+export const calculateAvailableBeds = (totalBeds, severeCases) => {
+  const available = totalBeds * 0.35;
+  return Math.trunc(available - severeCases);
+};
+
+export const estimator = (
+  {
+    region,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
+  },
+  useCaseFigure
+) => {
+  // Challenge 1
+  const currentlyInfected = reportedCases * useCaseFigure;
+  const lockDownDays = calculateLockDownDays(periodType, timeToElapse);
+  const factor = Math.trunc(lockDownDays / 3);
+  const infectionsByRequestedTime = Math.trunc(currentlyInfected * 2 ** factor);
+
+  //   Challenge 2
+  const severeCasesByRequestedTime = Math.trunc(
+    0.15 * infectionsByRequestedTime
+  );
+  const hospitalBedsByRequestedTime = calculateAvailableBeds(
+    totalHospitalBeds,
+    severeCasesByRequestedTime
+  );
+  //   Challenge 3
+
+  return {
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime
+  };
+};
